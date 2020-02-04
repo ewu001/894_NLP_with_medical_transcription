35521c6e79503da0942a88486dbad34244e8a342
ewu001 <59420306+ewu001@users.noreply.github.com>
2020-02-03 22:02:18 -0500

    Add ETL pipeline todo list

--- pipeline/extract transform pl.py
+++ pipeline/extract transform pl.py
@@ -16,6 +16,14 @@ def extract_process_csv(read_path, export_path):
     processeddf.to_csv(export_path, index=False)
     return
 
+# TO DO: enhance pre processor pipeline to include:
+# 1: remove HTML tags
+# 2: accented character convert
+# 3: remove stop words
+# 4: lemmalization
+# 5: remove punctuation
+
+
 if __name__ == '__main__':
     read_path = os.path.join(os.path.dirname(__file__), "../warehouse/medical_transcription_data_overall.csv")
     export_path = os.path.join(os.path.dirname(__file__), "../warehouse/keyword_target_data.csv")
