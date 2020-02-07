fc57b15753db60329567006451126cab9961a44a
ewu001 <59420306+ewu001@users.noreply.github.com>
2020-02-06 22:18:59 -0500

    Check in model description

--- estimator Attention/attention_estimator_model.py
+++ estimator Attention/attention_estimator_model.py
@@ -28,7 +28,7 @@ def attention_model(input_dim, input_length, learning_rate, embedding_dim, lstm_
     # Reshape to match LSTM's output shape, so that we can do element-wise multiplication  with LSTM output.
     # Stack attention network on top of LSTM layer
     # Reconstruct output layer to match many-to-one structure
-    
+
     attention = tf.keras.layers.Dense(1, activation='tanh')(activations)
     attention = tf.keras.layers.Flatten()(attention)
     attention = tf.keras.layers.Activation('softmax')(attention)
@@ -41,38 +41,3 @@ def attention_model(input_dim, input_length, learning_rate, embedding_dim, lstm_
     model = tf.keras.Model(inputs=md_input, outputs=output)
 
     return model
-
-
-'''
-Model summary: 
-__________________________________________________________________________________________________
-Layer (type)                    Output Shape         Param #     Connected to
-==================================================================================================
-input_1 (InputLayer)            [(None, 500)]        0
-__________________________________________________________________________________________________
-embedding (Embedding)           (None, 500, 200)     4319000     input_1[0][0]
-__________________________________________________________________________________________________
-lstm (LSTM)                     (None, 500, 32)      29824       embedding[0][0]
-__________________________________________________________________________________________________
-dense (Dense)                   (None, 500, 1)       33          lstm[0][0]
-__________________________________________________________________________________________________
-flatten (Flatten)               (None, 500)          0           dense[0][0]
-__________________________________________________________________________________________________
-activation (Activation)         (None, 500)          0           flatten[0][0]
-__________________________________________________________________________________________________
-repeat_vector (RepeatVector)    (None, 32, 500)      0           activation[0][0]
-__________________________________________________________________________________________________
-permute (Permute)               (None, 500, 32)      0           repeat_vector[0][0]
-__________________________________________________________________________________________________
-concatenate (Concatenate)       (None, 500, 64)      0           permute[0][0]
-                                                                 lstm[0][0]
-__________________________________________________________________________________________________
-lstm_1 (LSTM)                   (None, 32)           12416       concatenate[0][0]
-__________________________________________________________________________________________________
-output (Dense)                  (None, 2)            66          lstm_1[0][0]
-==================================================================================================
-Total params: 4,361,339
-Trainable params: 42,339
-Non-trainable params: 4,319,000
-__________________________________________________________________________________________________
-'''
